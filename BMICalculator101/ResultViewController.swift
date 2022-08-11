e22182da91d5c337a25e6bb71ace46de14763f47
M. Can Devecioğlu <can.devecioglu@gmail.com>
2022-08-11 22:54:23 +0300

    Segue and background image are implemented.

---
+++ BMICalculator101/ResultViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ResultViewController.swift
+//  BMICalculator101
+//
+//  Created by M. Can Devecioğlu on 11.08.2022.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        
+        self.dismiss(animated: true)
+        
+    }
+
+
+}
